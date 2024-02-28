de62f82379994ee1a51f668be29d3a928bf22bd2
Thamireslima9 <thamires.silva@aluno.ifsp.edu.br>
2024-02-28 11:13:08 -0300

    finalização dos 3 primeiros exercicios propostos.

--- ATV2_CJ3022099/Program.cs
+++ ATV2_CJ3022099/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             // projeto 1 
+            /*
             float novo_salario, salario;
 
             Console.WriteLine("Qual é o salário do funcionário?");
@@ -47,11 +48,104 @@
 
 
             //lista (exercicio 1)
+            Console.WriteLine();
+            float metros, centi, mili;
 
+            Console.WriteLine("Digite um valor em metros:");
+                metros = float.Parse(Console.ReadLine());
 
+            centi = metros * 100;
+            mili = metros * 1000;
 
+            Console.WriteLine("O valor em centimetros será de {0} e em milimetros será de {1}", centi, mili);
+
+            //Lista(exercicio 2)
+            Console.WriteLine();
+
+            //lista (Exercicio 3)
+            Console.WriteLine();
+            float peso, altura, imc;
+
+            Console.WriteLine("Primeiramente informe a sua altura:");
+                altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Agora insira o seu peso:");
+                peso = float.Parse(Console.ReadLine());
+
+            imc= peso / (float)Math.Pow(altura,2);
+            Console.WriteLine("O imc (Índice de Massa Corporal) é:{0:F2}", imc);
+
+            //lista (Exercicio 4)
+            Console.WriteLine();
+            
+            float nu1, nu2, nu3, pe1, pe2, pe3, media1, media2, media3;
+
+            Console.WriteLine("Digite três números separados por enter:");
+                nu1 = float.Parse(Console.ReadLine());
+                nu2 = float.Parse(Console.ReadLine());
+                nu3 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite três pesos refentes a mesma ordem dos números digitados,também separados por enter:");
+                pe1 = float.Parse(Console.ReadLine());
+                pe2 = float.Parse(Console.ReadLine());
+                pe3 = float.Parse(Console.ReadLine());
+
+            media1 = (nu1 * pe1) + (nu2 * pe2) + (nu3 * pe3);
+            media2 = pe1 + pe2 + pe3;
+            media3 = media1 / media2;
+
+            Console.WriteLine("O resultado da media ponderada é:{0:F2}",media3);
 
             
+            //lista (Exercicio 5)
+            Console.WriteLine();
+
+            //lista (Exercicio 7)
+            Console.WriteLine();
+            float real, dolar, cotacao;
+
+            Console.WriteLine("Digite um valor em real (R$):");
+                real = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da contação do dólar:");
+                dolar = float.Parse(Console.ReadLine());
+
+            cotacao = real / dolar;
+            Console.WriteLine("O valor correspondente em dólares é de:{0:F2}", cotacao);
+            
+            //lista (Exercicio 9)
+            Console.WriteLine();
+
+            */
+            //lista (Exercicio 11)
+            Console.WriteLine();
+
+            float nume,quadra, cubo;
+
+            Console.WriteLine("Digite um número:");
+                nume = float.Parse(Console.ReadLine());
+
+            quadra = Math.Pow
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         }
